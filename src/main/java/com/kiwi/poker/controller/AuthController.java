55dc0eaaa4439472d6f9c73f912e1dea00cebc62
Li Hao <yfzm1223@sjtu.edu.cn>
2019-05-14 23:55:52 +0800

    add database support

---
+++ src/main/java/com/kiwi/poker/controller/AuthController.java
@@ -0,0 +1,10 @@
+package com.kiwi.poker.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+}
