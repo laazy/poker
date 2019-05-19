bd979567e1d5430ed01ddf36ebf9533198b624a5
Veiasai <917971502@qq.com>
2019-05-19 22:43:04 +0800

    add h2 database for developing add swagger ui

--- src/main/java/com/kiwi/poker/PokerApplication.java
+++ src/main/java/com/kiwi/poker/PokerApplication.java
@@ -4,10 +4,35 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
 public class PokerApplication {
+    @Bean
+    public Docket createRestApi(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.kiwi.poker.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("TexasPoker")
+                .description("spring-mvc restful")
+                .version("1.0")
+                .build();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(PokerApplication.class, args);
